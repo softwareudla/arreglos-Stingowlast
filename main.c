ee58e0b2e5dea6ea275de0b35e7253bfdc43cbcc
Stingowlast <alejoj7caro0@gmail.com>
2025-04-09 07:57:42 -0500

    inicio 1

--- main.c
+++ main.c
@@ -3,7 +3,29 @@
 int main()
 {
 
+    int i, j;
+    float calificaciones[5][3];
+    float promedio_estudiantes[5];
+    float promedio_asignatura[3];
+    float maxestudiantes[5], minestudiantes[5];
+    float maxasignatura[3], minasignatura[3];
+    int aprobados[3] = {0}, reprobados[3];
+
+    for ( i = 0; i < 5; i++)
+    {
+        for ( j = 0; i < 3; j++)
+        {
+            do
+            {
+                printf("Ingrese la calificación del estudiante %d en asignatura %d :", i+1, j+1);
+                scanf("%f", &calificaciones[i][j]);
+            } while (calificaciones[i][j] < 0 || calificaciones[i][j] > 10);
+            
+        }
+        
+    }
     
+
 
     return 0;
 }
